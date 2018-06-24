43d3753a80c477db57804a5d742764c0e492e47e
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-06-24 06:40:18 +0100

    Create basic Widget pulling through essential data

--- wp-content/plugins/benyon-calendar/benyon-calendar.php
+++ wp-content/plugins/benyon-calendar/benyon-calendar.php
@@ -13,13 +13,49 @@ define( 'BENYON_CAL_PLUGIN_FILE_PATH', __FILE__);
 define( 'BENYON_CAL_PATH_PLUGIN_BASE', __DIR__);
 define( 'BENYON_CAL_INCLUDES', BENYON_CAL_PATH_PLUGIN_BASE . '/inc');
 
-try {
-    $result = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/" . get_field('benyon_cal_id', 'option') . "/events?key=" . get_field('benyon_cal_api_key', 'option'));
-} catch(Exception $e) {
-    error_log("FAIL!");
-    $result = false;
-}
-
 error_log("result" . var_export($result, true));
 
 include BENYON_CAL_INCLUDES . '/options_page.php';
+
+
+// register My_Widget
+add_action( 'widgets_init', function(){
+    register_widget( 'My_Widget' );
+});
+
+class My_Widget extends WP_Widget {
+    public $rawData;
+
+    // class constructor
+    public function __construct() {
+        $widget_ops = array(
+            'classname' => 'my_widget',
+            'description' => 'A plugin for Kinsta blog readers',
+        );
+        parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+
+        try {
+            $response = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/" . get_field('benyon_cal_id', 'option') . "/events?key=" . get_field('benyon_cal_api_key', 'option'));
+            $this->rawData = json_decode($response, true)['items'];
+        } catch(Exception $e) {
+            error_log("FAIL!");
+            $this->rawData = array();
+        }
+    }
+
+    // output the widget content on the front-end
+    public function widget( $args, $instance ) {
+        foreach ($this->rawData as &$value) {
+            echo "<div>" . json_encode($value['summary']) . "</div>";
+            echo "<div>" . json_encode($value['start']['dateTime']) . "</div>";
+            echo "<div>" . json_encode($value['end']['dateTime']) . "</div>";
+        }
+    }
+
+    // output the option form field in admin Widgets screen
+    public function form( $instance ) {
+    }
+
+    // save options
+    public function update( $new_instance, $old_instance ) {}
+}
