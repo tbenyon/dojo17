03b5f954f193154bbeed0e5d2c8413c95be94277
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-06-24 07:40:28 +0100

    Allow calendar max results in widget settings

--- wp-content/plugins/benyon-calendar/benyon-calendar.php
+++ wp-content/plugins/benyon-calendar/benyon-calendar.php
@@ -17,7 +17,6 @@ error_log("result" . var_export($result, true));
 
 include BENYON_CAL_INCLUDES . '/options_page.php';
 
-
 // register My_Widget
 add_action( 'widgets_init', function(){
     register_widget( 'My_Widget' );
@@ -33,18 +32,30 @@ class My_Widget extends WP_Widget {
             'description' => 'A plugin for Kinsta blog readers',
         );
         parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+    }
+
+    // output the widget content on the front-end
+    public function widget( $args, $instance ) {
+        $numResults = 6;
+        if ( isset( $instance[ 'amountToView' ] ) ) {
+            $numResults = intval($instance[ 'amountToView' ]);
+        }
+        $numResults += 1; //Google Max results seems to return 1 less than the max result
+
+        $calendarEntries = 'maxResults=' . $numResults;
 
         try {
-            $response = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/" . get_field('benyon_cal_id', 'option') . "/events?key=" . get_field('benyon_cal_api_key', 'option'));
+            $response = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/" . get_field('benyon_cal_id', 'option') . "/events?key=" . get_field('benyon_cal_api_key', 'option') . "&" . $calendarEntries);
             $this->rawData = json_decode($response, true)['items'];
         } catch(Exception $e) {
             error_log("FAIL!");
             $this->rawData = array();
         }
-    }
 
-    // output the widget content on the front-end
-    public function widget( $args, $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            echo "<h3>" . $instance[ 'title' ] . "</h3>";
+        }
+
         foreach ($this->rawData as &$value) {
             echo "<div>" . json_encode($value['summary']) . "</div>";
             echo "<div>" . json_encode($value['start']['dateTime']) . "</div>";
@@ -54,8 +65,39 @@ class My_Widget extends WP_Widget {
 
     // output the option form field in admin Widgets screen
     public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'New title', 'wpb_widget_domain' );
+        }
+
+        if ( isset( $instance[ 'amountToView' ] ) ) {
+            $amountToView = $instance[ 'amountToView' ];
+        }
+        else {
+            $amountToView = __( '6', 'wpb_widget_domain' );
+        }
+// Widget admin form
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id( 'amountToView' ); ?>"><?php _e( 'Number of Entries to Show:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'amountToView' ); ?>" name="<?php echo $this->get_field_name( 'amountToView' ); ?>" type="number" value="<?php echo esc_attr( $amountToView ); ?>" />
+        </p>
+        <?php
     }
 
+
     // save options
-    public function update( $new_instance, $old_instance ) {}
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['amountToView'] = ( ! empty( $new_instance['amountToView'] ) ) ? strip_tags( $new_instance['amountToView'] ) : '';
+        return $instance;
+    }
 }
