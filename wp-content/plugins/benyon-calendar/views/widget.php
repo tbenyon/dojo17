e67144d096170f0beed7279fcea70cfedee3ed48
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-08-28 17:10:02 +0100

    Add error handling to benyon-calendar plugin

--- wp-content/plugins/benyon-calendar/views/widget.php
+++ wp-content/plugins/benyon-calendar/views/widget.php
@@ -1,14 +1,18 @@
-<aside class='widget benyon-calendar-widget-container'>
+<?php if (count($events) > 0) : ?>
+  <aside class='widget benyon-calendar-widget-container'>
 
-    <?php if ( isset( $instance[ 'title' ] ) && $instance[ 'title' ] !== "" ) : ?>
-        <h3> <?php echo $instance[ 'title' ] ?></h3>
-    <?php endif; ?>
+      <?php if ( isset( $instance[ 'title' ] ) && $instance[ 'title' ] !== "" ) : ?>
+          <h3> <?php echo $instance[ 'title' ] ?></h3>
+      <?php endif; ?>
 
-    <?php foreach ($events as $event) : ?>
-        <div class='event <?php echo ($event['cancellation']) ? "event-type-cancellation" : NULL; ?>'>
-            <div class='event-title'><?php echo $event['event_summary']; ?></div>
-            <div class='event-times'><?php echo $event['event_date_time']; ?></div>
-        </div>
-    <?php endforeach; ?>
+      <?php foreach ($events as $event) : ?>
+          <div class='event <?php echo ($event['cancellation']) ? "event-type-cancellation" : NULL; ?>'>
+              <div class='event-title'><?php echo $event['event_summary']; ?></div>
+              <div class='event-times'><?php echo $event['event_date_time']; ?></div>
+          </div>
+      <?php endforeach; ?>
 
-</aside>
+  </aside>
+    <?php else : ?>
+      <?php error_log('Benyon Calendar Found no events.'); ?>
+<?php endif; ?>
