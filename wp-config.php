37319392b5950add0685451674c48dfbd9205a00
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-09-03 17:45:45 +0100

    Tidy wp-config

--- wp-config.php
+++ wp-config.php
@@ -18,12 +18,16 @@
  * @package WordPress
  */
 
-define('WP_HOME', getenv('WP_URL'));
-define('WP_SITEURL', getenv('WP_URL'));
-
+// ** Create custom Location for error logs ** //
 @ini_set('log_errors','On'); // enable or disable php error logging (use 'On' or 'Off')
 @ini_set('display_errors','Off'); // enable or disable public display of errors (use 'On' or 'Off')
 @ini_set('error_log','/var/www/html/dojo_php.log'); // path to server-writable log file
+
+
+// ** Site Base Settings - Used to overwrite WordPress DB setting ** //
+define('WP_HOME', getenv('WP_URL'));
+define('WP_SITEURL', getenv('WP_URL'));
+
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
